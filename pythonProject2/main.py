552d8c5e21e39c0266ad51b0f1cacc17047346d0
Datdudee6969 <93957010+Datdudee6969@users.noreply.github.com>
2023-05-03 10:01:29 -0400

    Pushit

---
+++ pythonProject2/main.py
@@ -0,0 +1,15 @@
+grade = int(input('Enter Students grade:'))
+
+if 90 <= grade <= 100:
+    print("Your Grade is A")
+elif 80 <= grade <= 89:
+    print("Your Grade is B")
+elif 70 <= grade <= 79:
+    print("Your Grade is C")
+elif 60 <= grade <= 69:
+    print("Your Grade is D")
+elif 50 <= grade <= 59:
+    print("Your Grade is D")
+
+else:
+    print("Invalid Input!")
